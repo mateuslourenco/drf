d2fb17fce704000e1602b3eb9f15cf5160bde6ac
Mateus Lourenço <mateuslourenco55@outlook.com>
2022-03-24 11:20:12 -0300

    criado serielizer

---
+++ tutorial/snippets/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+
+from tutorial.snippets.models import LANGUAGE_CHOICES, STYLE_CHOICES
+
+
+class SnippetSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    title = serializers.CharField(required=False, allow_blank=True, max_length=100)
+    code = serializers.CharField(style={'base_template': 'textarea.html'})
+    linenos = serializers.BooleanField(required=False)
+    language = serializers.ChoiceField(choices=LANGUAGE_CHOICES, default='python')
+    style = serializers.ChoiceField(choices=STYLE_CHOICES)
