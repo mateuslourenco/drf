52d44c87ff4367f45a98e8954f2a0cd8321246c4
Mateus Lourenço <mateuslourenco55@outlook.com>
2022-03-24 13:17:40 -0300

    corrigido migracao snippet

--- tutorial/snippets/serializers.py
+++ tutorial/snippets/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from tutorial.snippets.models import LANGUAGE_CHOICES, STYLE_CHOICES
+from tutorial.snippets.models import LANGUAGE_CHOICES, STYLE_CHOICES, Snippet
 
 
 class SnippetSerializer(serializers.Serializer):
@@ -9,4 +9,22 @@ class SnippetSerializer(serializers.Serializer):
     code = serializers.CharField(style={'base_template': 'textarea.html'})
     linenos = serializers.BooleanField(required=False)
     language = serializers.ChoiceField(choices=LANGUAGE_CHOICES, default='python')
-    style = serializers.ChoiceField(choices=STYLE_CHOICES)
+    style = serializers.ChoiceField(choices=STYLE_CHOICES, default='friendly')
+
+    def create(self, validated_data):
+        """
+        Created and return a new "Snippet" instance, given the validated data
+        """
+        return Snippet.objetcs.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        """
+        Update and return an existing "Snippet" instance, given the validated data
+        """
+        instance.title = validated_data.get('title', instance.title)
+        instance.code = validated_data.get('code', instance.code)
+        instance.linenos = validated_data.get('linenos', instance.linenos)
+        instance.language = validated_data.get('language', instance.language)
+        instance.style = validated_data.get('style', instance.style)
+        instance.save()
+        return instance
