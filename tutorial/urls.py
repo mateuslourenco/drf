7e0f47e573097969c6df41cecc28212b787f2878
Mateus Lourenço <mateuslourenco55@outlook.com>
2022-03-24 23:32:34 -0300

    serializers

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -19,6 +19,7 @@ from rest_framework import routers
 
 from tutorial.quickstart import views
 
+
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
@@ -26,5 +27,6 @@ router.register(r'groups', views.GroupViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('', include('tutorial.snippets.urls')),
 ]
